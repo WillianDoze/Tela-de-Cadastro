f7fdf816fb7c3597576c155a6a020681b26cd7e1
Willian da Silva Ferreira <104800185+WillianDoze@users.noreply.github.com>
2023-02-15 16:08:00 -0300

    att

--- js/login.php
+++ js/login.php
@@ -1,29 +1,43 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cadastro_cliente";
- 
+
 // Create connection
 $conn = new mysqli($servername,
     $username, $password, $dbname);
- 
+
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed:"
+    die("Connection failed: "
         . $conn->connect_error);
 }
- 
-$sqlquery = "INSERT INTO clientes (nome, CPF, RG, CEP, Endereco, Cidade, Estado, Numero, Email, foto) VALUES
-    ('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '$_POST[Email]')";
+
+$sqlquery = "INSERT INTO clientes VALUES
+    ('Willian da Silva Ferreira', '123456789-12', '23.236.563-5', '13170-575', 'Rua Barbara Blumer', 'Sumare', 'SP', '321', 'wfdirceu@gmail.com', 'file')"
+
 if ($conn->query($sqlquery) === TRUE) {
     echo "record inserted successfully";
 } else {
     echo "Error: " . $sqlquery . "<br>" . $conn->error;
 }
 
- 
+//  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+//     // collect value of input field
+//     $data = $_REQUEST['val1'];
+
+//     if (empty($data)) {
+//         echo "data is empty";
+//     } else {
+//         echo $data;
+//     }
+// }
+
+
 // Closing the connection.
-// $conn->close();
- 
-// ?> -->
+$conn->close();
+
+?>
