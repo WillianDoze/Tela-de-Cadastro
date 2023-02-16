0c0782e13dd59d1dcd8d5a49da60cc3721a911c3
Willian da Silva Ferreira <104800185+WillianDoze@users.noreply.github.com>
2023-02-16 16:51:42 -0300

    att de style

--- js/login.php
+++ js/login.php
@@ -30,16 +30,3 @@ $conn->close();
  
 ?>
 
-
- 
-<!-- // if ($_SERVER["REQUEST_METHOD"] == "POST") {
- 
-//     // collect value of input field
-//     $data = $_REQUEST['val1'];
- 
-//     if (empty($data)) {
-//         echo "data is empty";
-//     } else {
-//         echo $data;
-//     }
-// } -->
