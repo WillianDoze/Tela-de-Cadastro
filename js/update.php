2d2a1c6e25ad88387c9e39f5013ce341b26f578e
Willian da Silva Ferreira <104800185+WillianDoze@users.noreply.github.com>
2023-02-20 10:03:56 -0300

    att

--- js/update.php
+++ js/update.php
@@ -1,28 +1,20 @@
 <?php
-// Inclua o arquivo de conexão com o banco de dados
+
 require_once("conexao.php");
 
 // Capturar os novos valores dos campos
-$nome = $_POST['nome'];
-$cpf = $_POST['CPF'];
-$rg = $_POST['RG'];
-$cep = $_POST['CEP'];
-$endereco = $_POST['Endereco'];
-$cidade = $_POST['Cidade'];
-$uf = $_POST['Estado'];
-$numero = $_POST['Numero'];
-$email = $_POST['Email'];
-$senha = $_POST['Senha'];
-$foto = $_POST['foto'];
-
-// Atualizar os dados no banco de dados
-$sqlquery = "UPDATE clientes SET ('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '$_POST[Senha]', '$hora', '')";
-if ($conn->query($sqlquery) === TRUE) {
+date_default_timezone_set('America/Sao_Paulo');
+$hora = date('d/m/Y H:i:s');
+$sqlquery = "UPDATE clientes SET
+('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '$_POST[Senha]','$nome_foto', '$hora', '')";
+if ($conn->query($sqlquery)) {
+    $resultado = $conn->query($sqlquery);
+    $update = $resultado->fetch_assoc();
     echo "Dados atualizados com sucesso!";
 } else {
     echo "Error: " . $sqlquery . "<br>" . $conn->error;
 }
 
-// Fechar a conexão com o banco de dados
+
 $conn->close();
 ?>
