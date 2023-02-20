7046090d04117e7898bb24060cf6d09fcb6cc0cd
Willian da Silva Ferreira <104800185+WillianDoze@users.noreply.github.com>
2023-02-20 10:07:30 -0300

    att

--- js/update.php
+++ js/update.php
@@ -1,20 +1,20 @@
 <?php
 
-require_once("conexao.php");
+// require_once("conexao.php");
 
-// Capturar os novos valores dos campos
-date_default_timezone_set('America/Sao_Paulo');
-$hora = date('d/m/Y H:i:s');
-$sqlquery = "UPDATE clientes SET
-('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '$_POST[Senha]','$nome_foto', '$hora', '')";
-if ($conn->query($sqlquery)) {
-    $resultado = $conn->query($sqlquery);
-    $update = $resultado->fetch_assoc();
-    echo "Dados atualizados com sucesso!";
-} else {
-    echo "Error: " . $sqlquery . "<br>" . $conn->error;
-}
+// // Capturar os novos valores dos campos
+// date_default_timezone_set('America/Sao_Paulo');
+// $hora = date('d/m/Y H:i:s');
+// $sqlquery = "UPDATE clientes SET
+// ('$_POST[nome]', '$_POST[CPF]', '$_POST[RG]', '$_POST[CEP]', '$_POST[Endereco]', '$_POST[Cidade]', '$_POST[Estado]', '$_POST[Numero]', '$_POST[Email]', '$_POST[Senha]','$nome_foto', '$hora', '')";
+// if ($conn->query($sqlquery)) {
+//     $resultado = $conn->query($sqlquery);
+//     $update = $resultado->fetch_assoc();
+//     echo "Dados atualizados com sucesso!";
+// } else {
+//     echo "Error: " . $sqlquery . "<br>" . $conn->error;
+// }
 
 
-$conn->close();
-?>
+// $conn->close();
+// ?>
